128aabb3c783b5ebd70bedabb3db0a7a49218ee4
jlavoine <joe.lavoine@gmail.com>
2016-01-30 21:02:54 -0500

    Adding tutorial
    A very prototype tutorial just to give my testers something.

---
+++ DotR/Assets/Scripts/DotR/Combat/Chain/Tutorial/ProtoTutorial.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ProtoTutorial : MonoBehaviour {
+    public List<GameObject> Tutorials;
+
+    public GameObject CurrentTutorial;
+
+    private int m_nTutorial = 0;
+
+	// Use this for initialization
+	void Start () {
+        CurrentTutorial = Tutorials[m_nTutorial];
+        CurrentTutorial.SetActive( true );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ( Input.GetKeyDown( KeyCode.Escape ) )
+            Destroy( gameObject );
+	}
+
+    public void OnClick() {
+        m_nTutorial++;
+        if ( m_nTutorial >= Tutorials.Count ) {
+            Destroy( gameObject );
+            return;
+        }
+        else {
+            Destroy( CurrentTutorial );
+            CurrentTutorial = Tutorials[m_nTutorial];
+            CurrentTutorial.SetActive( true );
+        }
+    }
+}
