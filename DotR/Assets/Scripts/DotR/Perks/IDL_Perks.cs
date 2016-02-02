741f2fac04badaa7d70fafccee615f2ef2322f15
jlavoine <joe.lavoine@gmail.com>
2016-02-01 21:29:50 -0500

    Perks start
    Just the very start of perks - data file, IDL, data class

---
+++ DotR/Assets/Scripts/DotR/Perks/IDL_Perks.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+//////////////////////////////////////////
+/// IDL_Perks
+/// Loader for perks.
+//////////////////////////////////////////
+
+public abstract class IDL_Perks {
+    // dictionary of data
+    private static Dictionary<string, PerkData> m_dictData;
+
+    //////////////////////////////////////////
+    /// GetData()
+    /// Returns a perk's data by ID.
+    //////////////////////////////////////////
+    public static PerkData GetData( string i_strKey ) {
+        // if our dictionary of data is null, we must load it
+        if ( m_dictData == null ) {
+            m_dictData = new Dictionary<string, PerkData>();
+            DataUtils.LoadData<PerkData>( m_dictData, "Perks" );
+        }
+
+        // get the data and send an error if it's null
+        PerkData data = null;
+        if ( m_dictData.ContainsKey( i_strKey ) )
+            data = m_dictData[i_strKey];
+        else
+            Debug.LogError( "Looking for perk data with key " + i_strKey + " but not found!" );
+
+        return data;
+    }
+}
---
+++ DotR/Assets/Scripts/DotR/Perks/PerkData.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+//////////////////////////////////////////
+/// PerkData
+/// Immutable data for a perk.
+//////////////////////////////////////////
+
+public class PerkData : GenericData  {
+    // dictionary of perks and levels required for this perk
+    public Dictionary<string, int> PerkRequirements;
+
+    // the stat this perk affects
+    public string Stat;
+
+    // list of bonuses this perk applies (for each level)
+    public List<int> BonusList;
+
+    // list of XP requirements for this perk (for each level)
+    public List<int> ExpList;
+
+    public static PerkData GetExample() {
+        PerkData ex = new PerkData();
+
+        ex.ID = "PERK_1";
+        ex.PerkRequirements = new Dictionary<string, int>();
+        ex.PerkRequirements.Add( "Perk_2", 3 );
+        ex.Stat = "HP";
+        ex.BonusList = new List<int>();
+        ex.BonusList.Add( 5 );
+        ex.BonusList.Add( 6 );
+        ex.ExpList = new List<int>();
+        ex.ExpList.Add( 100 );
+
+        return ex;
+    }
+
+    public PerkData() {
+        PerkRequirements = new Dictionary<string, int>();
+    }
+}
